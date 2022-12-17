bff9a8137fabcf0ec61b1ea39688c8ade40c1de8
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2022-12-17 16:22:05 -0300

    Aulas Ajax

--- ajax/server.js
+++ ajax/server.js
@@ -7,4 +7,27 @@ app.use(express.static('.')) // Dentro da pasta atual na qual o arquivo server.j
 app.use(bodyParser.urlencoded({ extended: true })) // Se vinher algum formato a partir de um submit de um formulario esse código será responsável por ler os dados e transformar tudo em objeto
 app.use(bodyParser.json()) // Se vinher um JSON dentro do body da requisição esse código vai transformar o JSON em objeto 
 
+const multer = require('multer') // Vai ser usado para interpretar o formulario em que veio o arquivo do upload
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) { // Definir o diretorio de destino
+        callback(null, './upload')
+    },
+    filename: function (req, file, callback) { // Definir o nome do arquivo
+        callback(null, `${Date.now()}_${file.originalname}`)
+    }
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+    upload(req, res, err => {
+        if (err) { 
+            return res.end('Ocorreu um erro.')
+        }
+
+        res.end('Concluído com sucesso.')
+    })
+})
+
 app.listen(port, () => console.log(`Server running on port ${port}`))
