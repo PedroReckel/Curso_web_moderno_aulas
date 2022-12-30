5156d325f04edaae7d7d7a221d05156a951dd8e8
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2022-12-29 21:45:54 -0300

    Aulas Gulp

--- gulp_inicial/gulp/spa/gulp_tasks/app.js
+++ gulp_inicial/gulp/spa/gulp_tasks/app.js
@@ -1,19 +1,36 @@
 const gulp = require('gulp')
+const babel = require('gulp-babel')
+const uglifycss = require('gulp-uglifycss')
+const uglify = require('gulp-uglify')
+const concat = require('gulp-concat')
+const htmlmin = require('gulp-htmlmin')
+const sass = require('gulp-sass')(require('sass'))
 
 function appHTML(cb) {
-    return cb()
+    return gulp.src('src/**/*.html')
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest('build'))
 }
 
 function appCSS(cb) {
-    return cb()
+    return gulp.src('src/assets/sass/index.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(uglifycss({ "uglyComments": true }))
+        .pipe(concat('estilo.min.css'))
+        .pipe(gulp.dest('buid/assets/css'))
 }
 
 function appJS(cb) {
-    return cb()
+    return gulp.src('src/assets/js/**/*.js')
+        .pipe(babel({ presets: ['ENV'] }))
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('build/assets/js'))
 }
 
 function appIMG(cb) {
-    return cb()
+    return gulp.src('src/assets/imgs/**/*.*')
+        .pipe(gulp.dest('build/assets/imgs'))
 }
 
 module.exports = {
