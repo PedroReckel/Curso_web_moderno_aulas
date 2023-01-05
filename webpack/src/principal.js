3f12df398aeb4588c58cd372f0a78c3cfd7cf8cd
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2023-01-04 22:02:37 -0300

    Aulas Webpack

--- webpack/src/principal.js
+++ webpack/src/principal.js
@@ -1,7 +1,6 @@
 //const Pessoa = require('./pessoa')
 import Pessoa from './pessoa'
-import './modulos/moduloA'
-import './assets/css/estilo.css'
+import './assets/index.js'
 
 const atendente = new Pessoa
 console.log(atendente.cumprimentar())
--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,21 +1,37 @@
+const modoDev = process.env.NODE_ENV !== 'production' // Vai setar se a aplicação webpack vai rodar no modo de desenvolvimento ou produção. Se ele começar com o npm start a aplicação vai rodar em modo desenvolvimento e se eu rodar a aplicação com o npm build a aplicação vai rodar no modo produção
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js', // Arquivo de entrada nessa minha configuração do webpack
     output: {  // Configurando o arquivo e a pasta de saida
         filename: 'principal.js',
         path: __dirname + '/public' // O __dirname aponta para o diretorio raiz daonde o aquivo onde foi chamado está 
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader', // Adiciona CSS a DOM injetando a tag <style>
-                    'css-loader', // interpreta @import, url()...
-                ]
-            }
+    plugins: [new MiniCssExtractPlugin({
+        filename: "estilo.css"
+    })],
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({ // Minimizar js
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({}) // // Minimizar CSS
         ]
+    },
+    module: {
+        rules: [{
+                test: /\.s?[ac]ss$/,  // Responsável por ler todo arquivo .css
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    //'style-loader', // Adiciona CSS a DOM injetando a tag <style>
+                    'css-loader', // interpreta @import, url()...
+                    'sass-loader',
+                ]
+            }]
     }
 }
