63679322c1a6f30c62fb68ed551e67c712705dce
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2023-02-07 21:58:43 -0300

    Projeto calculadora com React

--- react/calculadora/src/components/Button.jsx
+++ react/calculadora/src/components/Button.jsx
@@ -1,5 +1,19 @@
-import React from "react"
+import React from 'react'
 import './Button.css'
 
-export default props => 
-    <button className="button">{props.label}</button>
+export default props => {
+    let classes = 'button'
+    // Se a propriedade operation for passada para o botão ele vai adicionar a classe operation se não vai retornar vazia
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+    
+    return (
+        // Sempre que eu tiver uma empressão com chaves dentro de uma arquivo jsx lá dentro eu posso colocar código JavaScript
+        <button 
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
+    ) 
+}
