783dd93cae7de779dc2a82e64169603b7102b3d1
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2023-02-11 17:22:23 -0300

    Projeto CRUD usando react

---
+++ react/crud/frontend/src/components/template/Main.jsx
@@ -0,0 +1,12 @@
+import './Main.css'
+import React from 'react'
+import Header from './Header'
+
+export default props => 
+    <React.Fragment>
+        <header>
+            <main className='content'>
+                Conteúdo
+            </main>
+        </header>
+    </React.Fragment>
---
+++ react/crud/frontend/src/components/template/Nav.jsx
@@ -0,0 +1,7 @@
+import './Nav.css'
+import React from 'react'
+
+export default props => 
+    <div className='menu-area'>
+        Navegação
+    </div>
