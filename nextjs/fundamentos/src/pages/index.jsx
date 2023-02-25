6ca9dda8c416684f4e01ede4de120898f518818e
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2023-02-25 16:36:37 -0300

    Aulas do módulo de Next.js

--- nextjs/fundamentos/src/pages/index.jsx
+++ nextjs/fundamentos/src/pages/index.jsx
@@ -1,11 +1,19 @@
-import Link from "next/link"
+import Navegador from "@/components/Navegador"
 
 export default function Inicio() {
   return (
-    <div>
-      <Link href="/estiloso">
-          Estiloso
-      </Link>
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      flexWrap: 'wrap',
+      height: '100vh'
+    }}>
+        <Navegador texto="Estiloso" destino="/estiloso" cor="crimson"/>
+        <Navegador texto="Exemplo" destino="/exemplo"/>
+        <Navegador texto="Navegação #01" destino="/navegacao" cor="green"/>
+        <Navegador texto="Navegação #02" destino="/cliente/sp-2/321" cor="#9400d3"/>
+        <Navegador texto="Componente com estado" destino="/estado" cor="pink"/>
     </div>
   )
 }
