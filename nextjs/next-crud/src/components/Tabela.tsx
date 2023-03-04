b9fc39e5e239d753e1fd854185f36eb3fb92fc11
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2023-03-03 21:38:33 -0300

    Aulas do módulo de Next.js

---
+++ nextjs/next-crud/src/components/Tabela.tsx
@@ -0,0 +1,44 @@
+import Cliente from "../core/Cliente"
+
+interface TabelaProps {
+    clientes: Cliente[]
+}
+
+export default function Tabela(props: TabelaProps) {
+
+    function renderizarCabecalho() {
+        return (
+            <tr>
+                <th className="text-left p-4">Código</th>
+                <th className="text-left p-4">Nome</th>
+                <th className="text-left p-4">Idade</th>
+            </tr>
+        )
+    }
+
+    function renderizarDados() {
+        return props.clientes?.map((cliente, i) => { // Ele só vai chamar o map() caso o array cliente estiver preenchido
+            return (
+                <tr key={cliente.id} className={`${i % 2 === 0 ? 'bg-purple-300' : 'bg-purple-200'}`}>
+                    <td className="text-left p-4">{cliente.id}</td>
+                    <td className="text-left p-4">{cliente.nome}</td>
+                    <td className="text-left p-4">{cliente.idade}</td>
+                </tr>
+            )
+        }) 
+    }
+
+    return ( 
+        <table className="w-full rounded-xl overflow-hidden">
+            <thead className={`
+                text-gray-100
+                bg-gradient-to-r from-purple-500 to bg-purple-800
+            `}>
+                {renderizarCabecalho()}
+            </thead>
+            <tbody>
+                {renderizarDados()}
+            </tbody>
+        </table>
+    )
+}
--- nextjs/next-crud/src/pages/index.tsx
+++ nextjs/next-crud/src/pages/index.tsx
@@ -1,7 +1,25 @@
+import Layout from "../components/Layout"
+import Cliente from "../core/Cliente"
+import Tabela from "../components/Tabela"
+
 export default function Home() {
+  
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Carlos', 23, '3'),
+    new Cliente('Pedro', 21, '4'),
+  ]
+  
   return (
-    <div className={``}>
-      <span className="text-4xl">Texto</span>
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-blue-500 to bg-purple-500
+      text-white
+    `}>
+      <Layout titulo="Cadastro Simples">
+          <Tabela clientes={clientes}></Tabela>
+      </Layout>
     </div>
   )
 }
