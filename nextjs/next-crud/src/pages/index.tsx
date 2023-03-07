41ac457bd5ba39afe7b8bdb98b5853334b588ce2
Pedro Reckel Roberte <84202423+PedroReckel@users.noreply.github.com>
2023-03-06 21:33:37 -0300

    Aulas do módulo de Next.js

--- nextjs/next-crud/src/pages/index.tsx
+++ nextjs/next-crud/src/pages/index.tsx
@@ -7,6 +7,9 @@ import { useState } from "react"
 
 export default function Home() {
   
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+
   const clientes = [
     new Cliente('Ana', 34, '1'),
     new Cliente('Bia', 45, '2'),
@@ -15,18 +18,23 @@ export default function Home() {
   ]
   
   function clienteSelecionado(Cliente: Cliente) {
-    console.log(`Alterando ${Cliente.nome}`)
+    setCliente(Cliente)
+    setVisivel('form')
   }
 
   function clienteExcluido(Cliente: Cliente) {
     console.log(`Excluindo ${Cliente.nome}`)
   }
 
-  function salvarcliente(cliente: Cliente) {
-    console.log(cliente)
+  function novoCliente() {
+    setCliente(Cliente.vazio())
+    setVisivel('form')
   }
 
-  const [visivel, setVisivel] = useState<'tabela' | 'form'>('tabela')
+  function salvarCliente(cliente: Cliente) {
+    console.log(cliente)
+    setVisivel('tabela')
+  }
 
   return (
     <div className={`
@@ -39,7 +47,7 @@ export default function Home() {
       <>
         <div className="flex justify-end">
           <Botao cor="green" className="mb-4" 
-            onClick={() => setVisivel('form')}>
+            onClick={novoCliente}>
             Novo cliente
           </Botao>
         </div>
@@ -50,8 +58,8 @@ export default function Home() {
       </>      
       ) : (
         <Formulario 
-          cliente={clientes[3]} 
-          clienteMudou={salvarcliente}
+          cliente={cliente} 
+          clienteMudou={salvarCliente}
           cancelado={() => setVisivel('tabela')}
         />
       )}
